b7f7cb4f410b2e9f8b148ddb1a2db810a7ae99a7
Felzarmt <juanfelipesalmon@gmail.com>
2025-04-02 19:07:16 -0500

    MOdulo finalizado

---
+++ real_estate/models/estate_property.py
@@ -0,0 +1,34 @@
+from odoo import models, fields
+
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Property"
+
+    name = fields.Char(required=True)
+    description = fields.Text()
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ("new", "New"),
+        ("offer_received", "Offer Received"),
+        ("offer_accepted", "Offer Accepted"),
+        ("sold", "Sold"),
+        ("canceled", "Canceled"),
+    ],required=True, copy=False, default="new")
+    postcode = fields.Char()
+    date_avaliability = fields.Date(copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
+    expected_price = fields.Float(required=True)
+    selling_price= fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([
+        ('north','North'),
+        ('south','South'),
+        ('east','East'),
+        ('west','West')
+])
